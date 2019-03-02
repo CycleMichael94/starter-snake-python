430b75ce29ee9ca96cde1dc1990ebddac917bf5d
Michael Hartmann <Michael@MacBook-Pro-5.hitronhub.home>
2019-03-02 00:19:03 -0800

    autodeploy

--- app/main.py
+++ app/main.py
@@ -3,47 +3,35 @@ import os
 import random
 import bottle
 
-from api import ping_response, start_response, move_response, end_response
+LEFT = 'left'
+RIGHT = 'right'
+UP = 'up'
+DOWN = 'down'
 
 @bottle.route('/')
 def index():
-    return '''
-    Battlesnake documentation can be found at
-       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
-    '''
+	return "<h1>I'm Nat and this is my snake</h1>"
 
 @bottle.route('/static/<path:path>')
 def static(path):
-    """
-    Given a path, return the static file located relative
-    to the static folder.
-
-    This can be used to return the snake head URL in an API response.
-    """
-    return bottle.static_file(path, root='static/')
+	return bottle.static_file(path, root='static/')
 
 @bottle.post('/ping')
 def ping():
-    """
-    A keep-alive endpoint used to prevent cloud application platforms,
-    such as Heroku, from sleeping the application instance.
-    """
-    return ping_response()
+	return {}
 
 @bottle.post('/start')
 def start():
-    data = bottle.request.json
+	return {
+		"color": '#AA22AA',
+		"taunt": "My first program, go nice"
+	}
 
-    """
-    TODO: If you intend to have a stateful snake AI,
-            initialize your snake state here using the
-            request's data if necessary.
-    """
-    print(json.dumps(data))
+@bottle.post('/end')
+def end():
+	return {}
 
-    color = "#00FF00"
 
-    return start_response(color)
 
 
 @bottle.post('/move')
@@ -57,11 +45,12 @@ def move():
     board = ['board']
     snakes = board['snakes']
     food = board['food']
+    enemies = set()
 
     for snake in snakes:
-        other_snake = snake['body']
-        for other_coords in other_snake:
-            print(other_coords)
+        enemy_location = snake['body']
+        enemies.append(enemy_location)
+
     for own_snake in body:
         pass
 
@@ -84,28 +73,18 @@ def move():
     #returns closest food coordinates and moves towards that
         #pass
 
-            return move_response(direction)
+	return {
+		"move": move
+	}
 
 
-@bottle.post('/end')
-def end():
-    data = bottle.request.json
 
-    """
-    TODO: If your snake AI was stateful,
-        clean up any stateful objects here.
-    """
-    print(json.dumps(data))
-
-    return end_response()
-
-# Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
 if __name__ == '__main__':
-    bottle.run(
-        application,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '8080'),
-        debug=os.getenv('DEBUG', True)
-    )
+	bottle.run(
+		application,
+		host=os.getenv('IP', '0.0.0.0'),
+		port=os.getenv('PORT', '8080'),
+		debug=os.getenv('DEBUG', True)
+	)
